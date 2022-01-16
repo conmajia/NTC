47beb9e489ddd2f04d1fc17b9bf2951aa1ab58fa
Greg <conmajia@live.com>
2022-01-16 13:27:09 +0800

    Added RAW
    Added the `raw` method to output space-saving sample values.

---
+++ NTC.cpp
@@ -0,0 +1,68 @@
+/*
+  NTC.cpp
+
+  Copyright (c) 2022, Greg Conmajia
+
+  This library is free software; you can redistribute it and/or
+  modify it under the terms of the GNU Lesser General Public
+  License as published by the Free Software Foundation; either
+  version 2.1 of the License, or (at your option) any later version.
+
+  This library is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+  Lesser General Public License for more details.
+
+  You should have received a copy of the GNU Lesser General Public
+  License along with this library; if not, write to the Free Software
+  Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+*/
+
+#include "NTC.h"
+#include "Arduino.h"
+
+// constructors
+NTC::NTC() {
+    this->_pin = NTC_NA;
+}
+NTC::NTC(int pin) {
+    this->_pin = pin;
+}
+NTC::NTC(int pin, int value) : NTC(pin) {
+    this->_ntc = value;
+}
+NTC::NTC(int pin, int value, int b) : NTC(pin, value) {
+    this->_b = b;
+}
+///////////////////////////////////
+// Public funtions
+///////////////////////////////////
+int NTC::raw() {
+    int raw = 0;
+    for (int i = 0; i < this->_repeat; i++) {
+        raw += analogRead((analog_pin_t)this->_pin);
+        if (this->_repeat > 1) delay(10);
+    }
+    return raw / this->_repeat;
+}
+float NTC::celsius() {
+    int raw = this->raw();
+    float average;
+    average = raw / this->_repeat;
+    // calculate for temperature
+    average = 1023 / average - 1;
+    average = this->_resistor / average;
+    // Steinhart-Hart Equation
+    // ARef = 5V
+    float celsius;
+    celsius = average / this->_ntc;
+    celsius = log(celsius);
+    celsius = celsius / this->_b;
+    celsius = celsius + 1.0 / (NTC_DEFAULT_T - NTC_ABSOLUTE_ZERO);
+    celsius = 1.0 / celsius;
+    celsius = celsius + NTC_ABSOLUTE_ZERO;
+    return celsius;
+}
+float NTC::fahrenheit() {
+    return (9.0 / 5.0) * celsius() + 32.0;
+}
---
+++ NTC.h
@@ -0,0 +1,39 @@
+/*
+ * ntc.h - Simple NTC thermistor library for arduino
+ * Greg Conmajia <conmajia@gmail.com>
+ * Jan. 7, 2022
+ * Released into the public domain.
+ */
+
+#ifndef _NTC_H_
+#define _NTC_H_
+
+#define NTC_ABSOLUTE_ZERO       -273.15             // Absolute Zero = -273.15 Celsius
+#define NTC_NaN                 0xffff              // Not a Number
+#define NTC_NA                  0xff                // Not Available
+#define NTC_DEFAULT_B           3450                // common B parameter: 3000 to 4000
+#define NTC_DEFAULT_T           25                  // nominal T parameter is almost always 25(C)
+#define NTC_DEFAULT_VALUE       10000               // resistance of the NTC
+#define NTC_DEFAULT_RESISTOR    10000               // series resistor in the voltage divider
+#define NTC_DEFAULT_REPEAT      5                   // repeated sample count of a single temperature read
+
+// Definition of the NTC sensor class
+class NTC {
+    public:
+        explicit NTC();                             // initializer
+        explicit NTC(int pin);                      // initializer (10K)
+        explicit NTC(int pin, int value);           // initializer with resistance value
+        explicit NTC(int pin, int value, int B);    // initializer with resistance value and B parameters
+        float celsius();                            // return current temperature in Celsius
+        float fahrenheit();                         // return current temperature in Fahrenheit
+        int raw();
+    private:
+        int _pin = NTC_NA;
+        int _b = NTC_DEFAULT_B;
+        int _t = NTC_DEFAULT_T;
+        int _repeat = NTC_DEFAULT_REPEAT;
+        int _ntc = NTC_DEFAULT_VALUE;
+        int _resistor = NTC_DEFAULT_RESISTOR;
+};
+
+#endif
